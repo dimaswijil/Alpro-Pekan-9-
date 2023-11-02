7fec060d33dac90c31e76e48470af0192ed8fabc
ikaameliaz <149253258+ikaameliaz@users.noreply.github.com>
2023-11-02 18:20:12 +0700

    Add files via upload

---
+++ TUGAS_2_ALPRO.py
@@ -0,0 +1,31 @@
+
+#function dengan nama jalan
+def jalan():
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    
+#function dengan nama jump    
+def jump():
+    jalan()
+    jalan()
+    jalan()
+    jalan()
+    jalan()
+    jalan()
+
+#perintah /memanggil fungsi
+jump()
+
+#cetak output
+print (output)
+
